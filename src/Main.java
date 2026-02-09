468a9a837276777aa45591922aacad802c200977
Octocat <ArmourIT21@gcc.edu>
2026-02-09 14:15:33 -0500

    Trivial change to test Git branch rule in master

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Testing git");
 
         for (int i = 1; i <= 5; i++) {
 
-            System.out.println("i= " + i);
+            System.out.println("i = " + i);
 
         }
     }
